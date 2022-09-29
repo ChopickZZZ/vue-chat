7e0677c4eb3169cc65d2e64228ae0ccc8b57cefd
Альберт Каримов <chopickzzz@gmail.com>
2022-09-29 21:09:56 +0300

    added static folder

---
+++ server/public/precache-manifest.b9e83909d4fc251739bb8a7f5f8f627d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef6cae2a5a6362cf0b0",
+    "url": "/css/app.3a77af13.css"
+  },
+  {
+    "revision": "d5075a7cd48ca592e543e164a6e643ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ef6cae2a5a6362cf0b0",
+    "url": "/js/app.5aa5d389.js"
+  },
+  {
+    "revision": "e38f1e7719bce294119c",
+    "url": "/js/chunk-vendors.a2d43df5.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
